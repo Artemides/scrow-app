f9e38af7d7ea02d3873ab7876856082af83da329
Artemides <edmpulasky@gmail.com>
2023-07-12 17:48:31 -0500

    initial setups

---
+++ scrow-client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,tsx,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
