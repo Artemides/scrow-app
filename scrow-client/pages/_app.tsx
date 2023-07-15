f56b33e462f5af174276312996df118a65c7b905
Artemides <edmpulasky@gmail.com>
2023-07-15 18:30:51 -0500

    someth ing

--- scrow-client/pages/_app.tsx
+++ scrow-client/pages/_app.tsx
@@ -1,8 +1,31 @@
+import { WagmiConfig, configureChains, createConfig, mainnet } from "wagmi";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { goerli } from "viem/dist/types/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { Titillium_Web } from "next/font/google";
+const titillium_web = Titillium_Web({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
+
+const { publicClient, webSocketPublicClient } = configureChains(
+  [goerli, mainnet],
+  [publicProvider()]
+);
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig config={config}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
 
 export default MyApp;
