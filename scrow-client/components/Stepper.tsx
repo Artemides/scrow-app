07cb400d1af8eb3894e04e3059f3d9634b58d717
Artemides <edmpulasky@gmail.com>
2023-07-18 13:00:46 -0500

    complete stepper form

--- scrow-client/components/Stepper.tsx
+++ scrow-client/components/Stepper.tsx
@@ -3,7 +3,7 @@ import { Button } from "../@/components/ui/button";
 import { GrFormPrevious, GrFormNext } from "react-icons/gr";
 import { useStepper } from "../hooks/useStepper";
 import { Input } from "../@/components/ui/input";
-const steps = ["beneficer", "Price", "abriter", "deploy"];
+const steps = ["beneficer", "Price", "arbiter", "deploy"];
 const Stepper = () => {
   const { currentStep, onNextStep, onPrevStep } = useStepper(steps);
   return (
@@ -26,7 +26,7 @@ const Stepper = () => {
           <label htmlFor="beneficiary-address">Address</label>
           <Input
             id="beneficiary-address"
-            className=" focus-visible:ring-offset-0 focus-visible:ring-1 focus-visible:ring-gray-600 bg-gray-800"
+            className=" focus-visible:ring-offset-0 focus-visible:ring-1 focus-visible:ring-neutral-600 bg-neutral-800"
             type="text"
             placeholder="Beneficiary Address"
           />
@@ -34,15 +34,34 @@ const Stepper = () => {
       )}
       {currentStep === 2 && (
         <div className="">
-          <label htmlFor="beneficiary-address">Good or Service value</label>
+          <label htmlFor="price-address">Good or Service value</label>
           <Input
-            id="beneficiary-address"
-            className=" focus-visible:ring-offset-0 focus-visible:ring-1 focus-visible:ring-gray-600 bg-gray-800"
-            type="text"
-            placeholder="Beneficiary Address"
+            id="price-address"
+            className=" focus-visible:ring-offset-0 focus-visible:ring-1 focus-visible:ring-neutral-500 bg-neutral-800"
+            type="number"
+            placeholder="ETH"
           />
         </div>
       )}
+
+      {currentStep === 3 && (
+        <div className="">
+          <label htmlFor="arbiter-address">Arbiter Address</label>
+          <Input
+            id="arbiter-address"
+            className="focus-visible:ring-offset-0 focus-visible:ring-1 focus-visible:ring-neutral-500 bg-neutral-800"
+            type="text"
+            placeholder="Arbiter Address"
+          />
+        </div>
+      )}
+
+      {currentStep === steps.length && (
+        <div className="flex justify-center">
+          <Button className="w-max">Deploy Contract</Button>
+        </div>
+      )}
+
       <div className="flex justify-center gap-2 items-center">
         <Button
           disabled={currentStep === 1}
