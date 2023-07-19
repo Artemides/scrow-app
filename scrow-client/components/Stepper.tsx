dbeab336ce7fd6ba3e3fb952fa5de38d30e8ec41
Artemides <edmpulasky@gmail.com>
2023-07-18 21:51:33 -0500

    deploy contract and handle input on change

--- scrow-client/components/Stepper.tsx
+++ scrow-client/components/Stepper.tsx
@@ -1,11 +1,64 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../@/components/ui/button";
 import { GrFormPrevious, GrFormNext } from "react-icons/gr";
 import { useStepper } from "../hooks/useStepper";
 import { Input } from "../@/components/ui/input";
+import Escrow from "../artifacts/contracts/Escrow.sol/Scrow.json";
+import { useChainId, useWaitForTransaction, useWalletClient } from "wagmi";
+import { Hex, TransactionReceipt, parseEther } from "viem";
+import { BigNumber } from "moralis/common-core";
 const steps = ["beneficer", "Price", "arbiter", "deploy"];
+type EscrowArgs = {
+  beneficiary: string;
+  arbiter: string;
+  price: bigint;
+};
+
 const Stepper = () => {
+  const chainId = useChainId();
+
   const { currentStep, onNextStep, onPrevStep } = useStepper(steps);
+  const { data: walletClient } = useWalletClient();
+  const [transactionHash, setTransactionHash] = useState<
+    `0x${string}` | undefined
+  >();
+  const [transactionReceipt, setTransactionReceipt] = useState<
+    TransactionReceipt | undefined
+  >();
+  const [contractAddress, setContractAddress] = useState<string | null>();
+  const [escrowArgs, setEscrowArgs] = useState<EscrowArgs>({
+    arbiter: "",
+    beneficiary: "",
+    price: BigInt(0),
+  });
+
+  useWaitForTransaction({
+    chainId,
+    hash: transactionHash,
+    onReplaced: ({ transactionReceipt }) => {
+      setTransactionReceipt(transactionReceipt);
+      const escrowAddres = transactionReceipt.contractAddress;
+      setContractAddress(escrowAddres);
+    },
+  });
+
+  const deployNewEscrowContract = async () => {
+    const abi = Escrow.abi;
+    const bytecode = Escrow.bytecode as Hex;
+    if (!walletClient) return;
+    const hash = await walletClient.deployContract({
+      abi,
+      bytecode,
+      args: [],
+    });
+    setTransactionHash(hash);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setEscrowArgs((prevArgs) => ({ ...prevArgs, [name]: value }));
+  };
+
   return (
     <>
       <div className="flex justify-between">
@@ -25,10 +78,13 @@ const Stepper = () => {
         <div className="">
           <label htmlFor="beneficiary-address">Address</label>
           <Input
+            onChange={handleChange}
             id="beneficiary-address"
+            name="beneficiary"
             className=" focus-visible:ring-offset-0 focus-visible:ring-1 focus-visible:ring-neutral-600 bg-neutral-800"
             type="text"
             placeholder="Beneficiary Address"
+            value={escrowArgs.beneficiary}
           />
         </div>
       )}
@@ -37,9 +93,11 @@ const Stepper = () => {
           <label htmlFor="price-address">Good or Service value</label>
           <Input
             id="price-address"
+            name="price"
             className=" focus-visible:ring-offset-0 focus-visible:ring-1 focus-visible:ring-neutral-500 bg-neutral-800"
             type="number"
             placeholder="ETH"
+            onChange={handleChange}
           />
         </div>
       )}
@@ -49,9 +107,12 @@ const Stepper = () => {
           <label htmlFor="arbiter-address">Arbiter Address</label>
           <Input
             id="arbiter-address"
+            name="arbiter"
             className="focus-visible:ring-offset-0 focus-visible:ring-1 focus-visible:ring-neutral-500 bg-neutral-800"
             type="text"
             placeholder="Arbiter Address"
+            value={escrowArgs.arbiter}
+            onChange={handleChange}
           />
         </div>
       )}
--- scrow-contracts/hardhat.config.ts
+++ scrow-contracts/hardhat.config.ts
@@ -2,27 +2,31 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
 import "dotenv/config";
-import "hardhat-deploy"
+import "hardhat-deploy";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
-  networks: {
-    hardhat: { chainId: 37337 },
-  },
-  namedAccounts:{
-    deployer:{
-      default:0,
-      37337:0
+    solidity: "0.8.18",
+    networks: {
+        hardhat: { chainId: 37337 },
     },
-    arbiter:{
-      default:1,
-      37337:1
+
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            37337: 0,
+        },
+        arbiter: {
+            default: 1,
+            37337: 1,
+        },
+        beneficiary: {
+            default: 2,
+            37337: 2,
+        },
     },
-    beneficiary:{
-      default:2,
-      37337:2
-    }
-  }
+    paths: {
+        artifacts: "../scrow-client/artifacts",
+    },
 };
 
 export default config;
